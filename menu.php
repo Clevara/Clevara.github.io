7b156f6b2d7e2213fc8a880d347e3b157e031703
daveaq <dave.green@aqovia.com>
2014-11-03 18:57:53 +0000

    Initial files for Clevara

---
+++ menu.php
@@ -0,0 +1,443 @@
+<?php
+echo '<div class="l-header">
+			<div class="l-header-h">
+
+				<div class="l-subheader at_top">
+					<div class="l-subheader-h i-cf">
+					
+						<div class="w-contacts">
+							<div class="w-contacts-h">
+								<div class="w-contacts-list">
+									<div class="w-contacts-item">
+										<i class="fa fa-phone"></i>
+										<span class="w-contacts-item-name">Phone:</span>
+										<span class="w-contacts-item-value">+44 207 242 2956</span>
+									</div>
+									<div class="w-contacts-item">
+										<i class="fa fa-envelope"></i>
+										<span class="w-contacts-item-name">Email:</span>
+										<span class="w-contacts-item-value"><a href="mailto:info@clevara.com">info@clevara.com</a></span>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						<div class="w-lang layout_dropdown show_onclick has_title">
+							<div class="w-lang-h">
+								<div class="w-lang-list">
+									<a class="w-lang-item lang_en" href="javascript:void(0);">
+										<span class="w-lang-item-icon"></span>
+										<span class="w-lang-item-title">Russian</span>
+									</a>
+									<a class="w-lang-item lang_de" href="javascript:void(0);">
+										<span class="w-lang-item-icon"></span>
+										<span class="w-lang-item-title">Deutsch</span>
+									</a>
+									<a class="w-lang-item lang_en" href="javascript:void(0);">
+										<span class="w-lang-item-icon"></span>
+										<span class="w-lang-item-title">French</span>
+									</a>
+								</div>
+								<div class="w-lang-current">
+									<span class="w-lang-item">
+										<span class="w-lang-item-icon"></span>
+										<span class="w-lang-item-title">English</span>
+									</span>
+								</div>
+							</div>
+						</div>
+
+						<div class="w-socials">
+							<div class="w-socials-h">
+								<div class="w-socials-list">
+
+									<div class="w-socials-item facebook">
+										<a class="w-socials-item-link" target="_blank" href="javascript:void(0);">
+											<i class="fa fa-facebook"></i>
+										</a>
+										<div class="w-socials-item-popup">
+											<div class="w-socials-item-popup-h">
+												<span class="w-socials-item-popup-text">Facebook</span>
+											</div>
+										</div>
+									</div>
+									<div class="w-socials-item twitter">
+										<a class="w-socials-item-link" target="_blank" href="javascript:void(0);">
+											<i class="fa fa-twitter"></i>
+										</a>
+										<div class="w-socials-item-popup">
+											<div class="w-socials-item-popup-h">
+												<span class="w-socials-item-popup-text">Twitter</span>
+											</div>
+										</div>
+									</div>
+									<div class="w-socials-item gplus">
+										<a class="w-socials-item-link" target="_blank" href="javascript:void(0);">
+											<i class="fa fa-google-plus"></i>
+										</a>
+										<div class="w-socials-item-popup">
+											<div class="w-socials-item-popup-h">
+												<span class="w-socials-item-popup-text">Google</span>
+											</div>
+										</div>
+									</div>
+									<div class="w-socials-item linkedin">
+										<a class="w-socials-item-link" target="_blank" href="javascript:void(0);">
+											<i class="fa fa-linkedin"></i>
+										</a>
+										<div class="w-socials-item-popup">
+											<div class="w-socials-item-popup-h">
+												<span class="w-socials-item-popup-text">LinkedIn</span>
+											</div>
+										</div>
+									</div>
+									<div class="w-socials-item youtube">
+										<a class="w-socials-item-link" target="_blank" href="javascript:void(0);">
+											<i class="fa fa-youtube-play"></i>
+										</a>
+										<div class="w-socials-item-popup">
+											<div class="w-socials-item-popup-h">
+												<span class="w-socials-item-popup-text">YouTube</span>
+											</div>
+										</div>
+									</div>
+									<div class="w-socials-item dribbble">
+										<a class="w-socials-item-link" target="_blank" href="javascript:void(0);">
+											<i class="fa fa-dribbble"></i>
+										</a>
+										<div class="w-socials-item-popup">
+											<div class="w-socials-item-popup-h">
+												<span class="w-socials-item-popup-text">Dribbble</span>
+											</div>
+										</div>
+									</div>
+
+								</div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+				<div class="l-subheader at_middle">
+					<div class="l-subheader-h i-cf">
+					
+						<!-- LOGO -->
+						<div class="w-logo">
+							<div class="w-logo-h">
+								<a class="w-logo-link" href="index.html">
+									<img class="w-logo-img" src="img/logo_0.png" alt="Impreza">
+									<span class="w-logo-title">
+										<span class="w-logo-title-h">Impreza</span>
+									</span>
+								</a>
+							</div>
+						</div>
+
+						<!-- SEARCH -->
+						<div class="w-search">
+							<div class="w-search-h">
+								<a class="w-search-show" href="javascript:void(0)"><i class="fa fa-search"></i></a>
+								<form class="w-search-form show_hidden" action="#">
+									<div class="w-search-form-h">
+										<div class="w-search-form-row">
+											<div class="w-search-label">
+												<label for="q">Just type and press 'enter'</label>
+											</div>
+											<div class="w-search-input">
+												<input id="q" type="text" value=""/>
+											</div>
+											<div class="w-search-submit">
+												<input type="submit" value="Search" />
+											</div>
+											<a class="w-search-close" href="javascript:void(0)" title="Close search"> &#10005; </a>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+
+						<!-- NAV -->
+						<nav class="w-nav layout_hor touch_disabled">
+							<div class="w-nav-control">
+								<i class="fa fa-bars"></i>
+							</div>
+							<ul class="w-nav-list level_1">
+								<li class="w-nav-item level_1 has_sublevel">
+									<a class="w-nav-anchor level_1" href="index.html">
+										<span class="w-nav-icon"><i class="fa fa-home"></i></span>
+										<span class="w-nav-title">Home</span>
+										<span class="w-nav-arrow"></span>
+									</a>
+									<!-- level 2 -->
+									<ul class="w-nav-list level_2">
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="home-fullscreen.html">
+												<span class="w-nav-title">Home - FullScreen Slider</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="home-landing.html">
+												<span class="w-nav-title">Home - Landing Page</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="home-parallax.html">
+												<span class="w-nav-title">Home - Parallax</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+									</ul>
+								</li>
+								<li class="w-nav-item level_1">
+									<a class="w-nav-anchor level_1" href="features.html">
+										<span class="w-nav-icon"><i class="fa fa-star-o"></i></span>
+										<span class="w-nav-title">Features</span>
+										<span class="w-nav-arrow"></span>
+									</a>
+								</li>
+								<li class="w-nav-item level_1 has_sublevel columns_4 active">
+									<a class="w-nav-anchor level_1" href="about.html">
+										<span class="w-nav-icon"><i class="fa fa-files-o"></i></span>
+										<span class="w-nav-title">Pages</span>
+										<span class="w-nav-arrow"></span>
+									</a>
+									<!-- level 2 -->
+									<ul class="w-nav-list level_2">
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="about.html">
+												<span class="w-nav-title">About Us</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="services.html">
+												<span class="w-nav-title">Services</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="team.html">
+												<span class="w-nav-title">Meet the Team</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="member.html">
+												<span class="w-nav-title">Team Member Page</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="pricing.html">
+												<span class="w-nav-title">Pricing</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="faq.html">
+												<span class="w-nav-title">FAQ Page</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="contact.html">
+												<span class="w-nav-title">Contact</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="contact-2.html">
+												<span class="w-nav-title">Contact 2</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="sidebar-right.html">
+												<span class="w-nav-title">Page Right Sidebar</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="sidebar-left.html">
+												<span class="w-nav-title">Page Left Sidebar</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2 active">
+											<a class="w-nav-anchor level_2" href="full-width.html">
+												<span class="w-nav-title">Page Full Width</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="404.html">
+												<span class="w-nav-title">404 Page</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+									</ul>
+								</li>
+								<li class="w-nav-item level_1 has_sublevel">
+									<a class="w-nav-anchor level_1" href="portfolio-3.html">
+										<span class="w-nav-icon"><i class="fa fa-picture-o"></i></span>
+										<span class="w-nav-title">Portfolio</span>
+										<span class="w-nav-arrow"></span>
+									</a>
+									<!-- level 2 -->
+									<ul class="w-nav-list level_2">
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="portfolio-4-fullwidth.html">
+												<span class="w-nav-title">Portfolio Full Width</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="portfolio-4.html">
+												<span class="w-nav-title">Portfolio 4 columns</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="portfolio-3.html">
+												<span class="w-nav-title">Portfolio 3 columns</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="portfolio-2.html">
+												<span class="w-nav-title">Portfolio 2 columns</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2 has_sublevel">
+											<a class="w-nav-anchor level_2" href="project-another-slider.html">
+												<span class="w-nav-title">Single Project</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+											<!-- level 3 -->
+											<ul class="w-nav-list level_3">
+												<li class="w-nav-item level_3">
+													<a class="w-nav-anchor level_3" href="project-slider.html">
+														<span class="w-nav-title">Single Project - Full Slider</span>
+														<span class="w-nav-arrow"></span>
+													</a>
+												</li>
+												<li class="w-nav-item level_3">
+													<a class="w-nav-anchor level_3" href="project-slider-23.html">
+														<span class="w-nav-title">Single Project - 2/3 Slider</span>
+														<span class="w-nav-arrow"></span>
+													</a>
+												</li>
+												<li class="w-nav-item level_3">
+													<a class="w-nav-anchor level_3" href="project-video.html">
+														<span class="w-nav-title">Single Project - Full Video</span>
+														<span class="w-nav-arrow"></span>
+													</a>
+												</li>
+												<li class="w-nav-item level_3">
+													<a class="w-nav-anchor level_3" href="project-video-23.html">
+														<span class="w-nav-title">Single Project - 2/3 Video</span>
+														<span class="w-nav-arrow"></span>
+													</a>
+												</li>
+												<li class="w-nav-item level_3">
+													<a class="w-nav-anchor level_3" href="project-image.html">
+														<span class="w-nav-title">Single Project - Full Image</span>
+														<span class="w-nav-arrow"></span>
+													</a>
+												</li>
+												<li class="w-nav-item level_3">
+													<a class="w-nav-anchor level_3" href="project-image-23.html">
+														<span class="w-nav-title">Single Project - 2/3 Image</span>
+														<span class="w-nav-arrow"></span>
+													</a>
+												</li>
+												<li class="w-nav-item level_3">
+													<a class="w-nav-anchor level_3" href="project-extended.html">
+														<span class="w-nav-title">Single Project - Extended</span>
+														<span class="w-nav-arrow"></span>
+													</a>
+												</li>
+											</ul>
+										</li>
+									</ul>
+								</li>
+								<li class="w-nav-item level_1 has_sublevel">
+									<a class="w-nav-anchor level_1" href="blog-grid.html">
+										<span class="w-nav-icon"><i class="fa fa-comments-o"></i></span>
+										<span class="w-nav-title">Blog</span>
+										<span class="w-nav-arrow"></span>
+									</a>
+									<!-- level 2 -->
+									<ul class="w-nav-list level_2">
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="blog-grid.html">
+												<span class="w-nav-title">Blog Grid</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="blog-large.html">
+												<span class="w-nav-title">Blog Large Image</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="blog-small.html">
+												<span class="w-nav-title">Blog Small Image</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="blog-post.html">
+												<span class="w-nav-title">Single Blog Post</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+									</ul>
+								</li>
+								<li class="w-nav-item level_1 has_sublevel">
+									<a class="w-nav-anchor level_1" href="elements.html">
+										<span class="w-nav-icon"><i class="fa fa-code"></i></span>
+										<span class="w-nav-title">Shortcodes</span>
+										<span class="w-nav-arrow"></span>
+									</a>
+									<!-- level 2 -->
+									<ul class="w-nav-list level_2">
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="elements.html">
+												<span class="w-nav-title">Elements</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="media.html">
+												<span class="w-nav-title">Media</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="typography.html">
+												<span class="w-nav-title">Typography</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="columns.html">
+												<span class="w-nav-title">Columns</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+									</ul>
+								</li>
+							</ul>
+						</nav>
+
+					</div>
+				</div>
+
+			</div>
+		</div>';
+?>
