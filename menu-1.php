b08e89116b3df5ba171cdabf6e47b9dcad6e2c71
Dave <dave@crowdbass.com>
2014-11-04 16:08:43 +0000

    updates to the Clevara site including bug fixes and updated IA

---
+++ menu-1.php
@@ -0,0 +1,226 @@
+<?php echo
+		"<div class="l-header">
+			<div class="l-header-h">
+
+				<div class="l-subheader at_top">
+					<div class="l-subheader-h i-cf">
+						<!-- CONTACT -->
+						<div class="w-contacts">
+							<div class="w-contacts-h">
+								<div class="w-contacts-list">
+									<div class="w-contacts-item">
+										<i class="fa fa-phone"></i>
+										<span class="w-contacts-item-name">Phone:</span>
+										<span class="w-contacts-item-value">+44 207 242 2956</span>
+									</div>
+									<div class="w-contacts-item">
+										<i class="fa fa-envelope"></i>
+										<span class="w-contacts-item-name">Email:</span>
+										<span class="w-contacts-item-value"><a href="mailto:info@clevara.com">info@clevara.com</a></span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- /CONTACT -->
+						
+						<!-- SOCIALS -->
+						<div class="w-socials">
+							<div class="w-socials-h">
+								<div class="w-socials-list">
+
+									<div class="w-socials-item facebook">
+										<a class="w-socials-item-link" target="_blank" href="javascript:void(0);">
+											<i class="fa fa-facebook"></i>
+										</a>
+										<div class="w-socials-item-popup">
+											<div class="w-socials-item-popup-h">
+												<span class="w-socials-item-popup-text">Facebook</span>
+											</div>
+										</div>
+									</div>
+									<div class="w-socials-item twitter">
+										<a class="w-socials-item-link" target="_blank" href="javascript:void(0);">
+											<i class="fa fa-twitter"></i>
+										</a>
+										<div class="w-socials-item-popup">
+											<div class="w-socials-item-popup-h">
+												<span class="w-socials-item-popup-text">Twitter</span>
+											</div>
+										</div>
+									</div>
+									<div class="w-socials-item gplus">
+										<a class="w-socials-item-link" target="_blank" href="javascript:void(0);">
+											<i class="fa fa-google-plus"></i>
+										</a>
+										<div class="w-socials-item-popup">
+											<div class="w-socials-item-popup-h">
+												<span class="w-socials-item-popup-text">Google</span>
+											</div>
+										</div>
+									</div>
+									<div class="w-socials-item linkedin">
+										<a class="w-socials-item-link" target="_blank" href="javascript:void(0);">
+											<i class="fa fa-linkedin"></i>
+										</a>
+										<div class="w-socials-item-popup">
+											<div class="w-socials-item-popup-h">
+												<span class="w-socials-item-popup-text">LinkedIn</span>
+											</div>
+										</div>
+									</div>
+									<div class="w-socials-item youtube">
+										<a class="w-socials-item-link" target="_blank" href="javascript:void(0);">
+											<i class="fa fa-youtube-play"></i>
+										</a>
+										<div class="w-socials-item-popup">
+											<div class="w-socials-item-popup-h">
+												<span class="w-socials-item-popup-text">YouTube</span>
+											</div>
+										</div>
+									</div>
+									<div class="w-socials-item dribbble">
+										<a class="w-socials-item-link" target="_blank" href="javascript:void(0);">
+											<i class="fa fa-dribbble"></i>
+										</a>
+										<div class="w-socials-item-popup">
+											<div class="w-socials-item-popup-h">
+												<span class="w-socials-item-popup-text">Dribbble</span>
+											</div>
+										</div>
+									</div>
+
+								</div>
+							</div>
+						</div>
+						<!-- /SOCIALS -->
+						
+					</div>
+				</div>
+				<div class="l-subheader at_middle">
+					<div class="l-subheader-h i-cf">
+					
+						<!-- LOGO -->
+						<div class="w-logo">
+							<div class="w-logo-h">
+								<a class="w-logo-link" href="index.html">
+									<img class="w-logo-img" src="img/logo_0.png" alt="Clevara">
+									<span class="w-logo-title">
+										<span class="w-logo-title-h">Clevara</span>
+									</span>
+								</a>
+							</div>
+						</div>
+
+						<!-- SEARCH -->
+						<div class="w-search">
+							<div class="w-search-h">
+								<a class="w-search-show" href="javascript:void(0)"><i class="fa fa-search"></i></a>
+								<form class="w-search-form show_hidden" action="#">
+									<div class="w-search-form-h">
+										<div class="w-search-form-row">
+											<div class="w-search-label">
+												<label for="q">Just type and press 'enter'</label>
+											</div>
+											<div class="w-search-input">
+												<input id="q" type="text" value=""/>
+											</div>
+											<div class="w-search-submit">
+												<input type="submit" value="Search" />
+											</div>
+											<a class="w-search-close" href="javascript:void(0)" title="Close search"> &#10005; </a>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+
+						<!-- NAV -->
+						<nav class="w-nav layout_hor touch_disabled">
+							<div class="w-nav-control">
+								<i class="fa fa-bars"></i>
+							</div>
+							<ul class="w-nav-list level_1">
+								<li class="w-nav-item level_1 has_sublevel active">
+									<a class="w-nav-anchor level_1" href="index.html">
+										<span class="w-nav-icon"><i class="fa fa-home"></i></span>
+										<span class="w-nav-title">Home</span>
+										<span class="w-nav-arrow"></span>
+									</a>
+								</li>
+								<li class="w-nav-item level_1 has_sublevel">
+									<a class="w-nav-anchor level_1" href="k12.html">
+										<span class="w-nav-icon"><i class="fa fa-files-o"></i></span>
+										<span class="w-nav-title">Learning Solutions</span>
+										<span class="w-nav-arrow"></span>
+									</a>
+									<!-- level 2 -->
+									<ul class="w-nav-list level_2">
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="solutions/k12.html">
+												<span class="w-nav-title">K-12</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="solutions/he.html">
+												<span class="w-nav-title">Higher Ed</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="solutions/professional-services.html">
+												<span class="w-nav-title">Employers</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="solutions/skills-for-work.html">
+												<span class="w-nav-title">Skills For Work Programmes</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="solutions/contprov.html">
+												<span class="w-nav-title">Content Providers</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+										<li class="w-nav-item level_2">
+											<a class="w-nav-anchor level_2" href="solutions/compliance.html">
+												<span class="w-nav-title">Compliance</span>
+												<span class="w-nav-arrow"></span>
+											</a>
+										</li>
+									</ul>
+								</li>
+								<li class="w-nav-item level_1 has_sublevel">
+									<a class="w-nav-anchor level_1" href="the-platform.html">
+										<span class="w-nav-icon"><i class="fa fa-picture-o"></i></span>
+										<span class="w-nav-title">Why Clevara?</span>
+										<span class="w-nav-arrow"></span>
+									</a>
+								</li>
+								<li class="w-nav-item level_1 has_sublevel">
+									<a class="w-nav-anchor level_1" href="team.html">
+										<span class="w-nav-icon"><i class="fa fa-comments-o"></i></span>
+										<span class="w-nav-title">The Team</span>
+										<span class="w-nav-arrow"></span>
+									</a>
+								</li>
+								<li class="w-nav-item level_1 has_sublevel">
+									<a class="w-nav-anchor level_1" href="contact-2.html">
+										<span class="w-nav-icon"><i class="fa fa-code"></i></span>
+										<span class="w-nav-title">Contact Us</span>
+										<span class="w-nav-arrow"></span>
+									</a>
+								</li>
+							</ul>
+						</nav>
+						<!-- /NAV -->
+
+					</div>
+				</div>
+
+			</div>
+		</div>";
+		?>
